f183ba9c45d077a42c9be7de201e037b61089855
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-08-27 15:41:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
+let newID = oldID + 1000000000;
+
+let ageIsValid = Number.isInteger(currentAge);
+
+let randomNumber = Math.floor(Math.random() * 19);
+
+let randomInteger = Math.floor(randomNumber);
+
+let randomUserID = randomInteger + 1000000001;
+
 /*
 Variables defined in the test suite:
 - oldID
